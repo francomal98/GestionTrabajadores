5868a44e042f850d085ebdadb1e457a52e5821fd
FrancoPDX <franco.maldonado@paradox-et.com>
2023-09-10 22:33:14 -0600

    Api para el proyecto personal de gestion de trabajadores

---
+++ Api/GestionTrabajadores/Interfaces/IAccount/IAccount.cs
@@ -0,0 +1,15 @@
+﻿using GestionTrabajadores.Models.AccountModel;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GestionTrabajadores.Interfaces.IAccount
+{
+    public interface IAccount
+    {
+        IActionResult Login([FromBody] AccountModels account);
+        IActionResult SignUp([FromBody] CreateAccount account);
+        IActionResult RecoveryPassword([FromHeader] string usuario);
+        IActionResult DeleteAccount();
+        IActionResult UpdateInfoAccount([FromBody] UpdateAccount account);
+
+    }
+}
---
+++ Api/GestionTrabajadores/Services/GeneratorIdService.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+
+namespace GestionTrabajadores.Services
+{
+    public class GeneratorIdService
+    {
+        public string GenerateIdentifier()
+        {
+            Random random = new Random();
+            string characters = "+-_#!&*@?0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            int lengthString = 8;
+            StringBuilder result = new StringBuilder(lengthString);
+
+            for(int i = 0; i < characters.Length; i++)
+            {
+                int index = random.Next(0, characters.Length);
+                result.Append(characters[index]);
+            }
+
+            return result.ToString();
+        }
+    }
+}
